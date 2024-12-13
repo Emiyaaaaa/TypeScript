20307a2afa19c87be339124cc2a72e82ecdc51d9
lihaozheng001 <lihaozheng001@realsee.com>
2024-12-13 17:31:46 +0800

    wip

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 type DataTypeThree<T> = {
-	one: Array<{two: Array<{three: T[]}>}>;
+	one2: Array<{two: Array<{three: T[]}>}>;
 };
 
 const testingThree: DataTypeThree<number> = {} as any as DataTypeThree<number>;
-const t3a: DataTypeThree<string>['one'] = testingThree.one;
+const t3a: DataTypeThree<string>['one2'] = testingThree['one2'];
+
+type StringArray = string[]
+type NumberArray = number[]
+
+const t3b2: StringArray = [] as NumberArray;
